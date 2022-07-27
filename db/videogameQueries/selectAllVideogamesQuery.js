c5eb4b7e42b8bac22aecdd598b2c53f9fb9cbd57
nyxelviqueira <ramon.viqueira@hotmail.com>
2022-07-27 18:13:44 +0200

    segundo commit

--- db/videogameQueries/selectAllVideogamesQuery.js
+++ db/videogameQueries/selectAllVideogamesQuery.js
@@ -23,12 +23,12 @@ const selectAllVideogamesQuery = async (idUser, keyword = '') => {
             V.releaseDate,
             U.username, 
             SUM(IFNULL(L.value = true, 0)) AS likes, 
-            V.idUser = 1 AS owner, 
-            BIT_OR(L.idUser = 1 AND L.value = 1) AS likedByMe
+            V.idUser = ? AS owner, 
+            BIT_OR(L.idUser = ? AND L.value = 1) AS likedByMe
         FROM videogames V
         LEFT JOIN likes L ON V.id = L.idVideogame
         LEFT JOIN users U ON V.idUser = U.id
-        WHERE V.title LIKE "%Hogwarts%"
+        WHERE V.title LIKE ?
         GROUP BY V.id
         ORDER BY V.releaseDate DESC
             `,
